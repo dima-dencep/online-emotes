cddfc37bd019325e7c8ce7a7969bc4c2f7ee7e99
dima_dencep <dima_dencep@redlance.org>
2025-03-17 19:46:48 +0700

    Fix remap on fabric

--- common/src/main/java/org/redlance/dima_dencep/mods/online_emotes/mixins/ClientMethodsMixin.java
+++ common/src/main/java/org/redlance/dima_dencep/mods/online_emotes/mixins/ClientMethodsMixin.java
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = PlatformTools.class, remap = false)
+@Mixin(PlatformTools.class)
 public abstract class ClientMethodsMixin {
 
     @Inject(
