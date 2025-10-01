b2c5697c1d5622f8123791b4353f7ad700d64a93
dima_dencep <dima_dencep@redlance.org>
2025-10-01 17:07:23 +0700

    Port to 1.21.9

--- forge/src/main/java/org/redlance/dima_dencep/mods/online_emotes/neoforge/OnlineEmotesPlatformImpl.java
+++ forge/src/main/java/org/redlance/dima_dencep/mods/online_emotes/neoforge/OnlineEmotesPlatformImpl.java
@@ -10,7 +10,7 @@
 
 package org.redlance.dima_dencep.mods.online_emotes.neoforge;
 
-import net.neoforged.fml.loading.LoadingModList;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.moddiscovery.ModFileInfo;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,14 +19,14 @@ import java.nio.file.Path;
 @SuppressWarnings("unused")
 public class OnlineEmotesPlatformImpl {
     public static String getModVersion(String modid) {
-        ModFileInfo info = LoadingModList.get().getModFileById(modid);
+        ModFileInfo info = FMLLoader.getCurrent().getLoadingModList().getModFileById(modid);
         if (info == null) return modid.toUpperCase() + "-UNKNOWN-NEOFORGE";
         return info.versionString();
     }
 
     @SuppressWarnings("UnstableApiUsage")
     public static @Nullable Path getModFile(String modid) {
-        ModFileInfo inf = LoadingModList.get().getModFileById(modid);
+        ModFileInfo inf = FMLLoader.getCurrent().getLoadingModList().getModFileById(modid);
         if (inf == null) return null;
         return inf.getFile().getFilePath();
     }
