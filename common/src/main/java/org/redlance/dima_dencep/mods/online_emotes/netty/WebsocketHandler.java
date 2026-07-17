e572417fb6323e0f5f15508e7c545a7502623c87
dima_dencep <dima_dencep@redlance.org>
2026-07-18 03:13:30 +0700

    Update emotecraft

--- common/src/main/java/org/redlance/dima_dencep/mods/online_emotes/netty/WebsocketHandler.java
+++ common/src/main/java/org/redlance/dima_dencep/mods/online_emotes/netty/WebsocketHandler.java
@@ -14,6 +14,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.zigythebird.playeranimcore.PlayerAnimLib;
 import io.github.kosmx.emotes.common.network.EmotePacket;
+import io.github.kosmx.emotes.common.network.PacketBound;
 import io.github.kosmx.emotes.mc.McUtils;
 import net.minecraft.core.RegistryAccess;
 import org.redlance.dima_dencep.mods.online_emotes.OnlineEmotes;
@@ -59,7 +60,7 @@ public class WebsocketHandler extends SimpleChannelInboundHandler<WebSocketFrame
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame msg) {
         switch (msg) {
-            case BinaryWebSocketFrame frame -> this.proxy.receiveMessage(new EmotePacket(frame.content()));
+            case BinaryWebSocketFrame frame -> this.proxy.receiveMessage(new EmotePacket(frame.content(), PacketBound.CLIENT));
 
             case TextWebSocketFrame frame -> {
                 JsonElement element = PlayerAnimLib.GSON.fromJson(frame.text(), JsonElement.class);
